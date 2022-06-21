1cdc8d944798087f3dcdaa38a81e8f50e31ed8b1
Sumeet Agarwal <sumeet.a@gmail.com>
2022-06-21 16:26:20 -0700

    implicit return isn't actually working for string literals

--- main.tcl
+++ main.tcl
@@ -1,14 +1,16 @@
 proc !help {} {
-    "Bot commands: !project !discord !pair. Let me know if you have any coding questions, or just want me to look at something on-stream."
+    return "Bot commands: !project !discord !pair. Let me know if you have any coding questions, or just want me to look at something on-stream."
 }
 
-proc !pair {} { "Let's pair together on-stream! I can work together over the following platforms: Discord, VS Code Live Share, JetBrains Code With Me, Replit" }
+proc !pair {} {
+  return "Let's pair together on-stream! I can work together over the following platforms: Discord, VS Code Live Share, JetBrains Code With Me, Replit" 
+}
 
 proc !project {} {
-    "Twitch bot in Pure C https://github.com/sumeet/twitchdrop"
+    return "Twitch bot in Pure C https://github.com/sumeet/twitchdrop"
 }
 proc !today {} { !project }
 
 proc !discord {} {
-    "Join the Sumcademy Discord server at https://discord.gg/5hHqbk6eRu"
+    return "Join the Sumcademy Discord server at https://discord.gg/5hHqbk6eRu"
 }
