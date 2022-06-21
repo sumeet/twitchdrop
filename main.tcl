688b2beec3da6fb3eee1e7c3cd99ecb7c0fc74b6
Sumeet Agarwal <sumeet.a@gmail.com>
2022-06-21 15:58:35 -0700

    change up the commands a bit

--- main.tcl
+++ main.tcl
@@ -1,15 +1,14 @@
-proc !commands {} {
-    return "!project !discord !help !pair"
+proc !help {} {
+    "Bot commands: !project !discord !pair. Let me know if you have any coding questions, or just want me to look at something on-stream."
 }
 
-proc !help {} {
-    return "If you think I might be able to help you code something, just ask anytime. Don't worry about interrupting. If you wanted a list of commands, you should type !commands"
-}
-proc !pair {} { return "Let's pair on something together on-stream! Either VSCode Live Share or Jetbrains, we'll figure out the logistics." }
+proc !pair {} { "Let's pair together on-stream! I can work together over the following platforms: Discord, VS Code Live Share, JetBrains Code With Me, Replit" }
+
 proc !project {} {
-    return "Learning C writing a Twitch bot -- https://github.com/sumeet/twitchdrop"
+    "Twitch bot in Pure C https://github.com/sumeet/twitchdrop"
 }
 proc !today {} { !project }
+
 proc !discord {} {
-    return "Join the Sumcademy Discord server at https://discord.gg/5hHqbk6eRu"
+    "Join the Sumcademy Discord server at https://discord.gg/5hHqbk6eRu"
 }
