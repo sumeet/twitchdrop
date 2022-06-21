0bac5abf3c2011040ff91e7a30340943b2b61a5e
Sumeet Agarwal <sumeet.a@gmail.com>
2022-06-20 18:30:54 -0700

    update !pair command

--- main.tcl
+++ main.tcl
@@ -3,9 +3,9 @@ proc !commands {} {
 }
 
 proc !help {} {
-    return "If you think I might be able to help you code something, just ask anytime. Don't worry about interrupting."
+    return "If you think I might be able to help you code something, just ask anytime. Don't worry about interrupting. If you wanted a list of commands, you should type !commands"
 }
-proc !pair {} { !help }
+proc !pair {} { return "Let's pair on something together on-stream! Either VSCode Live Share or Jetbrains, we'll figure out the logistics." }
 proc !project {} {
     return "Learning C writing a Twitch bot -- https://github.com/sumeet/twitchdrop"
 }
